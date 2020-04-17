910936b45b644cc092194e95c612c0604488ada1
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-04-17 15:36:27 +0700

    save

--- src/main/java/sh/now/arifikhsanudin/employeemanager/service/DatabaseService.java
+++ src/main/java/sh/now/arifikhsanudin/employeemanager/service/DatabaseService.java
@@ -5,10 +5,27 @@
  */
 package sh.now.arifikhsanudin.employeemanager.service;
 
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Arif Ikhsanudin
  */
 public class DatabaseService {
-    
+    public Connection connection;
+    public Statement statement;
+
+    public void config() {
+        try {
+            System.out.println("Congrats - Seems your MySQL JDBC Driver Registered!");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/netbeans_employee_manager", "root", "");
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
 }
---
+++ src/main/java/sh/now/arifikhsanudin/employeemanager/ui/MainContract.java
@@ -0,0 +1,6 @@
+package sh.now.arifikhsanudin.employeemanager.ui;
+
+public interface MainContract {
+    void initDatabase();
+    void populateView();
+}
